fb963060cde9f944ec9169bf0131d8efe3b713e8
SmoothAsphaltRoads <abhijnannanda4k.dpsmeg@gmail.com>
2026-09-15 00:05:21 +0530

    options panel + bg music

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,23 +3,32 @@ extends Node
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-@onready var start_button: Button = $VBoxContainer/Button
-@onready var options_button: Button = $VBoxContainer/Button2
-@onready var exit_button: Button = $VBoxContainer/Button3
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var start_button: Button = $MainButtons/Button
+@onready var options_button: Button = $MainButtons/Button2
+@onready var exit_button: Button = $MainButtons/Button3
+@onready var options: Panel = $Options
 
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_pressed)
 	options_button.pressed.connect(_on_options_pressed)
 	exit_button.pressed.connect(_on_exit_pressed)
+	main_buttons.visible = true
+	options.visible = false
 
 
 func _on_start_pressed() -> void:
 	LevelManager.start_game()
 
 func _on_options_pressed() -> void:
-	print("open options menu")
+	main_buttons.visible = false
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	main_buttons.visible = true
+	options.visible = false
