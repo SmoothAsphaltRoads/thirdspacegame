4f9b6c11af76adf0757a3408be41871f00c46e6a
Harish Rusum <harish.rusum@gmail.com>
2026-09-15 19:11:20 +0530

    added player death

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,29 +2,35 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_sound: AudioStreamPlayer2D = $"Jump sound"
+@onready var death_sound: AudioStreamPlayer2D = $"Death sound"
 
 const BASE_SPEED := 50.0
 const MAX_SPEED := 300.0
 const ACCELERATION := 600.0
 const FRICTION := 1400.0
 const JUMP_HEIGHT := -850.0
-const CUT_MULTIPLIER:= 1.0
-const MIN_JUMP_HEIGHT:= -300.0
-const COYOTE_TIME:= 0.15
-var COYOTE_TIMER:= 0.0
-const JUMP_BUFFER_TIME:= 0.15
-var JUMP_BUFFER_TIMER:= 0.0
-var TIME:= 0.0
+const CUT_MULTIPLIER := 1.0
+const MIN_JUMP_HEIGHT := -300.0
+const COYOTE_TIME := 0.15
+var COYOTE_TIMER := 0.0
+const JUMP_BUFFER_TIME := 0.15
+var JUMP_BUFFER_TIMER := 0.0
+
+var is_dead := false
+
 
 func _physics_process(delta: float) -> void:
+	if is_dead:
+		return
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		COYOTE_TIMER -= delta
 	else:
 		COYOTE_TIMER = COYOTE_TIME
-		
+
 	if Input.is_action_just_pressed("jump"):
-		JUMP_BUFFER_TIMER  = JUMP_BUFFER_TIME
+		JUMP_BUFFER_TIMER = JUMP_BUFFER_TIME
 	else:
 		JUMP_BUFFER_TIMER -= delta
 
@@ -38,10 +44,9 @@ func _physics_process(delta: float) -> void:
 		COYOTE_TIMER = 0.0
 		JUMP_BUFFER_TIMER = 0.0
 		jump_sound.play()
-	
+
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y = max(velocity.y * CUT_MULTIPLIER, MIN_JUMP_HEIGHT)
-	
 
 	var direction := Input.get_axis("left", "right")
 
@@ -50,10 +55,29 @@ func _physics_process(delta: float) -> void:
 			velocity.x = direction * BASE_SPEED
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0.0, FRICTION  * delta)
+		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
 
 	_update_animation(direction)
 	move_and_slide()
+	_check_enemy_collision()
+
+
+func _check_enemy_collision() -> void:
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		var collider := collision.get_collider()
+		if collider.is_in_group("enemies"):
+			die()
+			return
+
+func die() -> void:
+	death_sound.play(0.3)
+	is_dead = true
+	velocity = Vector2.ZERO
+	animated_sprite_2d.animation = "Hit"
+	await animated_sprite_2d.animation_finished
+	LevelManager.restart_level()
+	is_dead = false 
 
 
 func _update_animation(direction: float) -> void:
