958f030bded000d39f1c2551bf98f98345b960af
Harish Rusum <harish.rusum@gmail.com>
2026-09-15 18:41:55 +0530

    added very basic enemy

--- scenes/scale_control.gd
+++ scenes/scale_control.gd
@@ -2,7 +2,6 @@ extends OptionButton
 
 
 
-
 func _on_item_selected(index: int) -> void:
 	var options = [2, 1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25]
 	var value = options[index]
---
+++ scripts/enemy.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var detection_area: Area2D = $Area2D
+
+var SPEED := 100.0
+var ACCELERATION:= 200
+var FRICTION:= 600
+var target: Node2D = null
+
+
+func _ready() -> void:
+	detection_area.body_entered.connect(_on_body_entered)
+	detection_area.body_exited.connect(_on_body_exited)
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if target:
+		var direction := signf(target.global_position.x - global_position.x)
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION*delta)
+		_update_animation(direction)
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, FRICTION*delta)
+		_update_animation(0.0)
+	
+	move_and_slide()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		target = body
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body == target:
+		target = null
+
+func _update_animation(direction: float) -> void:
+	animated_sprite_2d.animation = "Idle_2"
+	if direction != 0.0:
+		animated_sprite_2d.flip_h = direction > 0.
