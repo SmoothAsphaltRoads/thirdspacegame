ce123bc04231b29a83827b37a93017eadb067c63
Harish Rusum <harish.rusum@gmail.com>
2026-09-18 14:58:20 +0530

    small bugfix

--- scripts/player.gd
+++ scripts/player.gd
@@ -28,6 +28,7 @@ func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		velocity.y = minf(velocity.y, 1200.0)
 		COYOTE_TIMER -= delta
 	else:
 		COYOTE_TIMER = COYOTE_TIME
@@ -59,7 +60,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
-
+	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+		print("vel=", velocity, " floor=", is_on_floor(), " wall=", is_on_wall(), " dead=", is_dead)
+	
 	_update_animation(direction)
 	move_and_slide()
 	_check_enemy_collision()
@@ -91,13 +94,19 @@ func _check_object_collision() -> void:
 			return
 
 func die() -> void:
-	death_sound.play(0.3)
-	is_dead = true  
+	print("die() called, is_dead=", is_dead)
+	if is_dead:
+		return
+	is_dead = true
 	velocity = Vector2.ZERO
-	animated_sprite_2d.animation = "Hit"
+	death_sound.play(0.3)
+	animated_sprite_2d.play("Hit")
+	print("playing: ", animated_sprite_2d.animation, " is_playing=", animated_sprite_2d.is_playing())
 	await animated_sprite_2d.animation_finished
+	print("animation done")
 	LevelManager.restart_level()
-	is_dead = false 
+	print("restart called")
+
 
 func _update_animation(direction: float) -> void:
 	if not is_on_floor():
