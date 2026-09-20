c9a851157e06e338ea315dd8bb64a4906aa510d2
SmoothAsphaltRoads <abhijnannanda4k.dpsmeg@gmail.com>
2026-09-21 00:49:26 +0530

    added infoblocks to all levels

--- static_body_2d.gd
+++ static_body_2d.gd
@@ -1,12 +1,17 @@
 extends StaticBody2D
+
+@export_multiline var info_text: String = "Default tip text"
+
 @onready var hit_detector: Area2D = $HitDetector
 @onready var sprite_2d: Sprite2D = $Sprite2D
-var is_hit: bool = false
 @onready var label: Label = $CanvasLayer/Label
-@export var info_text: String = "Press Jump to Leap!"
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+var is_hit: bool = false
+
 func _ready() -> void:
-	label.visible=false
+	label.text = info_text
+	label.visible = false
 
 func _on_hit_detector_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if is_hit:
@@ -16,14 +21,14 @@ func _on_hit_detector_body_shape_entered(body_rid: RID, body: Node2D, body_shape
 	label.visible = true
 	audio_stream_player_2d.play(0.0)
 
-func _input(event:InputEvent) ->void :
+func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
-		sprite_2d.visible = true
-		is_hit = false
-		label.visible = false
-
+		close_info()
 
 func _on_button_pressed() -> void:
+	close_info()
+
+func close_info() -> void:
 	sprite_2d.visible = true
 	is_hit = false
 	label.visible = false
